bf71be8477fae4dd6ffe93b962ba1a2c4b46177f
saito <saito@saitonoMacBook-Air.local>
2019-10-14 18:28:34 +0900

    なんとなくパラメーター修正

--- src/main.rs
+++ src/main.rs
@@ -28,14 +28,13 @@ use futures::future;
 
 async fn job((i, u): &(usize, usize)) -> usize {
   println!("begin {:?} => {:?}", i, u);
-  Delay::new(Duration::from_secs(*u as u64)).await;
+  Delay::new(Duration::from_millis(((*u) * 100) as u64)).await;
   println!("end {:?} => {:?}", i, u);
   *i
 }
 
 async fn dispatch(i: usize, requests: &[(usize, usize)]) -> Vec<usize> {
   let mut vec = vec![];
-  println!("id: {:?}", i);
   for request in requests.iter() {
     vec.push(job(request).await)
   }
@@ -45,10 +44,10 @@ async fn dispatch(i: usize, requests: &[(usize, usize)]) -> Vec<usize> {
 async fn run() {
   let mut durations = vec![];
   let mut futures = vec![];
-  for i in 0..30 {
-    durations.push((i, rand::random::<usize>() % 10));
+  for i in 0..100 {
+    durations.push((i, rand::random::<usize>() % 67));
   }
-  let Pool = 10;
+  let Pool = 30;
   let requests_chunk = durations.chunks(durations.len() / Pool);
   for (i, requests) in requests_chunk.enumerate() {
     futures.push(dispatch(i, requests).boxed());
